91a40e500207a8033460c51d282e48047a56d2db
FuzzyStatic <af732@drexel.edu>
2015-05-02 17:43:08 -0400

    Constants reorganized

--- src/com/fuzzycraft/fuzzy/constants/Defaults.java
+++ src/com/fuzzycraft/fuzzy/constants/Defaults.java
@@ -1,4 +1,4 @@
-package com.fuzzycraft.fuzzy.utilities;
+package com.fuzzycraft.fuzzy.constants;
 
 /**
  * 
@@ -6,12 +6,12 @@ package com.fuzzycraft.fuzzy.utilities;
  *
  */
 
-public class Constants {
+public class Defaults {
 	public static final String WORLD = "world_the_end";
 	public static final double X = 0;
 	public static final double Y = 20;
 	public static final double Z = 0;
-	public static final int TIME_RESPAWN = 432000; // 6 hours of ticks: 20 ticks/seconds * 21600 seconds (6 hours)
+	public static final int TIME = 432000; // 6 hours of ticks: 20 ticks/seconds * 21600 seconds (6 hours)
 	public static final String MSG = "The beast awakens from his slumber...";
 	public static final boolean CREATE_PORTAL = false;
 	public static final boolean CREATE_EGG = true;
---
+++ src/com/fuzzycraft/fuzzy/constants/Paths.java
@@ -0,0 +1,15 @@
+package com.fuzzycraft.fuzzy.constants;
+
+/**
+ * 
+ * @author FuzzyStatic (fuzzy@fuzzycraft.com)
+ *
+ */
+
+public class Paths {
+	public static final String LOCATION = "spawnLocation";
+	public static final String TIME = "respawnTime";
+	public static final String MSG = "spawnAlert";
+	public static final String CREATE_PORTAL = "createPortal";
+	public static final String CREATE_EGG = "createEgg";
+}
