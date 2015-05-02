070789328e338fccdbb8131da6990976523ffd53
FuzzyStatic <af732@drexel.edu>
2015-05-02 17:43:37 -0400

    Configuration

--- src/com/fuzzycraft/fuzzy/EnderdragonRespawner.java
+++ src/com/fuzzycraft/fuzzy/EnderdragonRespawner.java
@@ -5,9 +5,12 @@ import org.bukkit.World;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.fuzzycraft.fuzzy.constants.Defaults;
+import com.fuzzycraft.fuzzy.constants.Paths;
 import com.fuzzycraft.fuzzy.listeners.EnderdragonPreventPortal;
 import com.fuzzycraft.fuzzy.listeners.EnderdragonSpawnTimer;
-import com.fuzzycraft.fuzzy.utilities.Constants;
+import com.fuzzycraft.fuzzy.utilities.SerializableLocation;
+import com.fuzzycraft.fuzzy.utilities.YamlLocation;
 
 /**
  * 
@@ -25,18 +28,31 @@ public class EnderdragonRespawner extends JavaPlugin {
 	private Location location;
 	
 	public void onEnable() {
-		world = getServer().getWorld(Constants.WORLD);
-		location = new Location(world, Constants.X, Constants.Y, Constants.Z);
-				
-		// Create our object instances
-		es = new EnderdragonSpawner(this, world, location, Constants.MSG);
+		world = getServer().getWorld(Defaults.WORLD);
+		location = new Location(world, Defaults.X, Defaults.Y, Defaults.Z);
+		 
+		// Configuration setup.
+		getDataFolder().mkdir();
+		getConfig().addDefault(Paths.LOCATION, new SerializableLocation(location).serialize());
+		getConfig().addDefault(Paths.TIME, Defaults.TIME);
+		getConfig().addDefault(Paths.MSG, Defaults.MSG);
+		getConfig().addDefault(Paths.CREATE_PORTAL, Defaults.CREATE_PORTAL);
+		getConfig().addDefault(Paths.CREATE_EGG, Defaults.CREATE_EGG);
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+		
+		// Get location from configuration.
+		SerializableLocation sc = new SerializableLocation(new YamlLocation(getConfig(), (Paths.LOCATION)).getLocationMap());
+		
+		// Create our object instances.
+		es = new EnderdragonSpawner(this, world, sc.getLocation(), getConfig().getString(Paths.MSG));
 		ec = new EnderdragonChecker(world);
 		
-		// Create listener instances
-		est = new EnderdragonSpawnTimer(this, es, ec, Constants.TIME_RESPAWN);
-		epp = new EnderdragonPreventPortal(this, ec.world(), Constants.CREATE_PORTAL, Constants.CREATE_EGG);
+		// Create listener instances.
+		est = new EnderdragonSpawnTimer(this, es, ec, getConfig().getInt(Paths.TIME));
+		epp = new EnderdragonPreventPortal(this, ec.world(), getConfig().getBoolean(Paths.CREATE_PORTAL), getConfig().getBoolean(Paths.CREATE_EGG));
 
-		// Register listeners
+		// Register listeners.
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(est, this);
 		pm.registerEvents(epp, this);
---
+++ src/com/fuzzycraft/fuzzy/utilities/SerializableLocation.java
@@ -0,0 +1,79 @@
+package com.fuzzycraft.fuzzy.utilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+ 
+/**
+ * 
+ * @author FuzzyStatic (fuzzy@fuzzycraft.com)
+ *
+ */
+
+public class SerializableLocation implements ConfigurationSerializable {
+    
+	private final World world;
+	private final double x, y, z;
+	private final float yaw, pitch;
+	private final Location location;
+ 
+	/**
+	 * Location parameters from location.
+	 * @param location
+	 */
+    public SerializableLocation(Location location) {
+    	this.world = location.getWorld();
+    	this.x = location.getBlockX();
+    	this.y = location.getBlockY();
+    	this.z = location.getBlockZ();
+    	this.pitch = location.getPitch();
+    	this.yaw = location.getYaw();
+        this.location = location;
+    }
+ 
+    /**
+     * Location parameters from map.
+     * @param map
+     */
+    public SerializableLocation(Map<String, Object> map) {
+    	this.world = Bukkit.getWorld(map.get("world").toString());
+    	this.x = (Double) map.get("x");
+    	this.y = (Double) map.get("y");
+    	this.z = (Double) map.get("z");
+    	this.pitch = Float.intBitsToFloat((Integer) map.get("pitch"));    
+    	this.yaw = Float.intBitsToFloat((Integer) map.get("yaw"));
+    	this.location = new Location(world, x, y, z, yaw, pitch);
+    }
+ 
+    @Override
+    public Map<String, Object> serialize() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("world", location.getWorld().getName());
+        map.put("x", location.getX());
+        map.put("y", location.getY());
+        map.put("z", location.getZ());
+        map.put("pitch", Float.floatToIntBits(location.getPitch()));
+        map.put("yaw", Float.floatToIntBits(location.getYaw()));
+        return map;
+    }
+    
+    /**
+     * Return location.
+     * @return
+     */
+    public Location getLocation() {
+        return this.location;
+    }
+    
+    /**
+     * Return world location.
+     * @return
+     */
+    public World getWorld() {
+        return this.world;
+    }
+}
