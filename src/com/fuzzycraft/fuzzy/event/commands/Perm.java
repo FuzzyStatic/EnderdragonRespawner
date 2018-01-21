d2c60e8dfa2d9a83b77fbf5973e2859a6c4505c1
Allen Flickinger <allen.flickinger@gmail.com>
2018-01-20 22:47:24 -0500

    Switch to protected

--- src/com/fuzzycraft/fuzzy/event/commands/Perm.java
+++ src/com/fuzzycraft/fuzzy/event/commands/Perm.java
@@ -2,12 +2,12 @@
  * @Author: Allen Flickinger (allen.flickinger@gmail.com)
  * @Date: 2018-01-20 17:31:38
  * @Last Modified by: FuzzyStatic
- * @Last Modified time: 2018-01-20 18:37:17
+ * @Last Modified time: 2018-01-20 22:08:22
  */
 
 package com.fuzzycraft.fuzzy.event.commands;
 
 public class Perm {
-  public static final String START = "enderdragonrespawner.event.start";
-  public static final String STOP = "enderdragonrespawner.event.stop";
+  protected static final String START = "enderdragonrespawner.event.start";
+  protected static final String STOP = "enderdragonrespawner.event.stop";
 }
