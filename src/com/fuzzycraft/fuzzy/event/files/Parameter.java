138b501f4ec80cce17d2bf90732a9262405ddeff
Allen Flickinger <allen.flickinger@gmail.com>
2018-02-02 22:40:20 -0500

    Update default parameter

--- src/com/fuzzycraft/fuzzy/event/files/Parameter.java
+++ src/com/fuzzycraft/fuzzy/event/files/Parameter.java
@@ -2,7 +2,7 @@
  * @Author: Allen Flickinger (allen.flickinger@gmail.com)
  * @Date: 2018-01-20 18:08:07
  * @Last Modified by: FuzzyStatic
- * @Last Modified time: 2018-01-20 22:07:42
+ * @Last Modified time: 2018-02-02 20:45:22
  */
 
 package com.fuzzycraft.fuzzy.event.files;
@@ -16,7 +16,7 @@ public class Parameter {
   protected static final int AMOUNT = 1;
   protected static final int TIME = 21600; // In seconds
   protected static final double X = 0;
-  protected static final double Y = 20;
+  protected static final double Y = 100;
   protected static final double Z = 0;
   protected static final String MSG = "The beast awakens from its slumber...";
 }
